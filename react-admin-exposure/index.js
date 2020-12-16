8e90f708910bad715787f94aeceeacd1b9797237
Bernat Vadell Puigros <bvadell@feimsoft.com>
2020-12-16 10:48:42 +0100

    sample project

---
+++ react-admin-exposure/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const { AdurcBuilder } = require('@adurc/core');
+const { SqlServerDriver } = require('@adurc/driver-mssql');
+const { ReactAdminExposure } = require('@adurc/exposure-react-admin');
+const { GraphQLIntrospector } = require('@adurc/introspector-graphql');
+
+dotenv.config();
+
+async function bootstrap() {
+    const app = express();
+
+    app.use(express.json());
+
+    const adurc = await AdurcBuilder.create()
+        .use(new SqlServerDriver({
+            config: {
+                database: process.env.DB_DATABASE,
+                server: process.env.DB_SERVER,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                options: {
+                    instanceName: process.env.DB_INSTANCE,
+                }
+            }
+        }))
+        .use(new GraphQLIntrospector({
+            path: process.cwd() + '/models/*.graphql',
+            encoding: 'utf8',
+        }))
+        .build();
+
+    const reactAdmin = new ReactAdminExposure({
+        adurc,
+    });
+
+    reactAdmin.applyMiddleware({ app, path: '/graphql' });
+
+    app.listen(3000);
+
+    console.log(
+        'Serving the GraphQL Playground on http://localhost:3000/graphql',
+    );
+
+    return app;
+}
+
+bootstrap();
